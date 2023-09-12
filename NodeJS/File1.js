8ae6f8f5bd4cda8a19a7b30e29805379545d54fd
viki2207 <vidhishah2207@gmail.com>
2023-09-12 21:23:13 +1000

    Practice for recursion or oops

---
+++ NodeJS/File1.js
@@ -0,0 +1,16 @@
+const prompt = require("prompt-sync")();
+
+let n1 = prompt("enter First Number: ");
+
+let n2 = prompt("Enter Second Number: ");
+
+if (n1 > n2) {
+  console.log("n1 is greater");
+} else {
+  console.log("n2 is greater");
+}
+
+//while
+i = 0;
+while( 1)
+console.log( i +"India");
---
+++ NodeJS/iterationvsrecursion.js
@@ -0,0 +1,36 @@
+//Iteration mesans called function itself
+function PrintSeries(i) {
+  if (i > 5) return;
+  print(i);
+  PrintSeries(i + 1);
+}
+PrintSeries(1);
+
+//oops : c#, js, php
+//oops: object oreiented programing
+//object its a virtal
+//1.class
+//class pen{
+// String color;
+
+//}
+
+//object: defining attributes or instance variable
+//some functions call instance methods
+//new pen() ; its call object all properties and methods having whaever with class
+//oops pillar
+//abtractions: hide main things and display what we need (essential features of the system without get involved with the complex things )
+//class music player(){
+//start(); stop(), pause();
+//}
+
+//music player mp = new musicplayer();
+//mp.start();
+//inheritance: inherint from the base class
+//class puppy extends dogs(){}
+//encapsulation:holding everything together in a single unit example :capsule
+
+//class in capsule
+//pollymorphism: poly + morphism = some thing existing   in more than one form
+//pollymorphism
+//for polymorephism for function overloading and overriding
