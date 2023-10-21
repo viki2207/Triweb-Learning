fa62dda474faa7c2d2dddf9a3ab6680ed3da95ba
viki2207 <vidhishah2207@gmail.com>
2023-10-21 11:39:30 +1100

    Project Added

--- Projects/portfolio/src/components/Works/works.js
+++ Projects/portfolio/src/components/Works/works.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Button from "./Button";
 import "./works.css";
-import Portfolio1 from "../../assets/portfolio-1.png";
-import Portfolio2 from "../../assets/portfolio-2.png";
-import Portfolio3 from "../../assets/portfolio-3.png";
-import Portfolio4 from "../../assets/portfolio-4.png";
-import Portfolio5 from "../../assets/portfolio-5.png";
-import Portfolio6 from "../../assets/portfolio-6.png";
+import Portfolio1 from "../../assets/BookStore.png";
+import Portfolio2 from "../../assets/Tictactoe.png";
+import Portfolio3 from "../../assets/Devconnector.png";
+import Portfolio4 from "../../assets/Quizapp.png";
+import Portfolio5 from "../../assets/Calculator.png";
+import Portfolio6 from "../../assets/Userblock.png";
 
 const Works = () => {
   return (
@@ -16,13 +17,182 @@ const Works = () => {
         my work is without error. I am excited to bring my skills and experience
         to help business achieve their goals and create a strong presence
       </span>
-      <div className="worksImgs">
-        <img src={Portfolio1} alt="" className="worksImg" />
-        <img src={Portfolio2} alt="" className="worksImg" />
-        <img src={Portfolio3} alt="" className="worksImg" />
-        <img src={Portfolio4} alt="" className="worksImg" />
-        <img src={Portfolio5} alt="" className="worksImg" />
-        <img src={Portfolio6} alt="" className="worksImg" />
+      {/* worksImgs */}
+      <div className="container">
+        <div class="box">
+          <div class="imgBx">
+            <img src={Portfolio1} alt="" className="worksImg" />
+          </div>
+          <div className="content">
+            <div className="Animated">
+              <h2>Book Store</h2>
+              <p>Mern Project</p>
+              <a
+                href="https://github.com/viki2207"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <button type="button" className="AnimatedBtn">
+                  Live
+                </button>
+              </a>
+              <a
+                href="https://github.com/viki2207"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <button type="button" className="AnimatedBtn">
+                  Code
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="box">
+          <div class="imgBx">
+            <img src={Portfolio2} alt="" className="worksImg" />
+          </div>
+          <div className="content">
+            <div className="Animated">
+              <h2>TIC-TAC-TOE</h2>
+              <p>React Project</p>
+              <a
+                href="https://github.com/viki2207"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <button type="button" className="AnimatedBtn">
+                  Live
+                </button>
+              </a>
+              <a
+                href="https://github.com/viki2207"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <button type="button" className="AnimatedBtn">
+                  Code
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="box">
+          <div class="imgBx">
+            <img src={Portfolio3} alt="" className="worksImg" />
+          </div>
+          <div className="content">
+            <div className="Animated">
+              <h2>Devloper Connector</h2>
+              <p>Mern Project</p>
+              <a
+                href="https://github.com/viki2207"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <button type="button" className="AnimatedBtn">
+                  Live
+                </button>
+              </a>
+              <a
+                href="https://github.com/viki2207"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <button type="button" className="AnimatedBtn">
+                  Code
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="box">
+          <div class="imgBx">
+            <img src={Portfolio4} alt="" className="worksImg" />
+          </div>
+          <div className="content">
+            <div className="Animated">
+              <h2>Quiz Application</h2>
+              <p>Node JS Project</p>
+              <a
+                href="https://github.com/viki2207"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <button type="button" className="AnimatedBtn">
+                  Live
+                </button>
+              </a>
+              <a
+                href="https://github.com/viki2207"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <button type="button" className="AnimatedBtn">
+                  Code
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="box">
+          <div class="imgBx">
+            <img src={Portfolio5} alt="" className="worksImg" />
+          </div>
+          <div className="content">
+            <div className="Animated">
+              <h2>Calculator</h2>
+              <p>Node JS Project</p>
+              <a
+                href="https://github.com/viki2207"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <button type="button" className="AnimatedBtn">
+                  Live
+                </button>
+              </a>
+              <a
+                href="https://github.com/viki2207"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <button type="button" className="AnimatedBtn">
+                  Code
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="box">
+          <div class="imgBx">
+            <img src={Portfolio6} alt="" className="worksImg" />
+          </div>
+          <div className="content">
+            <div className="Animated">
+              <h2>User-Block</h2>
+              <p>Vanila Js Project</p>
+              <a
+                href="https://github.com/viki2207"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <button type="button" className="AnimatedBtn">
+                  Live
+                </button>
+              </a>
+              <a
+                href="https://github.com/viki2207"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <button type="button" className="AnimatedBtn">
+                  Code
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
       <button className="workBtn">See more</button>
     </section>
