4363181c8535386d8ab3a839ca3bcfe9cd4b82a6
viki2207 <vidhishah2207@gmail.com>
2023-11-02 20:38:48 +1100

    callback

---
+++ NodeJS/ItnerviewQuestions/callback.js
@@ -0,0 +1,7 @@
+/*
+Callback:async quivalent of a function
+helps in a non-blocking code
+helps in a performance of single thread application
+callback hell: call back function inside again callback called callback hell
+we need to break callback hell using await, async, promise using for break big function to make it small modules..
+*/
