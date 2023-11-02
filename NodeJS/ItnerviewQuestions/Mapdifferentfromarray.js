df367335e4b21817ce662944f6e6a4546e9ccce6
viki2207 <vidhishah2207@gmail.com>
2023-11-02 20:19:52 +1100

    diff between map and array

---
+++ NodeJS/ItnerviewQuestions/Mapdifferentfromarray.js
@@ -0,0 +1,16 @@
+/*
+    an Array is a collection of iteams at contiguous memory locations.
+    The idea is to store multiple items of the same type togather
+    starting point :offset
+    */
+
+let arr = [1, 2, 3, 4, 5, 6];
+/*
+A map is an associative container that sotres elements in a mapped array
+use to store records
+just tell them which wan to put in which array
+*/
+let mape = {
+  uname: "uname",
+  mobile: 1223232,
+};
