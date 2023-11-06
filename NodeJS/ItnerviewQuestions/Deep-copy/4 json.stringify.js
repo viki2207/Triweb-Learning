4f0926e6618b3c33ca51e6037837020fea81b2ad
viki2207 <vidhishah2207@gmail.com>
2023-11-06 21:06:22 +1100

    ramda and json stringify

---
+++ NodeJS/ItnerviewQuestions/Deep-copy/4 json.stringify.js
@@ -0,0 +1,7 @@
+const obj = { a: { p: 1, q: 2 }, b: 2, c: 3 };
+const newObj = JSON.parse(JSON.stringify(obj));
+console.log("Before - obj", obj);
+console.log("Before - obj", newObj);
+obj["a"]["p"] = 11;
+console.log("After - obj", obj);
+console.log("Before - obj", newObj);
