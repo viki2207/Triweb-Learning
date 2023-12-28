c0eed33f4c53950bc192a871132987fe9c46f08e
viki2207 <vidhishah2207@gmail.com>
2023-12-28 18:57:32 +1100

    result reducer

--- Projects/quiz_app_mern/client/src/Redux/store.js
+++ Projects/quiz_app_mern/client/src/Redux/store.js
@@ -7,7 +7,7 @@ import { resultReducer } from "./result_reducer";
 //combine and return as a central store
 const rootReducer = combineReducers({
   questions: questionReducer,
-  //result: resultReducer,
+  result: resultReducer,
 });
 //create store with reducer
 export default configureStore({ reducer: rootReducer });
