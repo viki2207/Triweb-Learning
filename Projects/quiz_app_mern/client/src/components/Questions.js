6694aa31a020786436c83009b83bb776bfdad644
viki2207 <vidhishah2207@gmail.com>
2024-01-08 19:38:12 +1100

    remove extra code and add script

--- Projects/quiz_app_mern/client/src/components/Questions.js
+++ Projects/quiz_app_mern/client/src/components/Questions.js
@@ -6,34 +6,6 @@ import { updateResultAction } from "../Redux/result_reducer";
 import { updateResult } from "../hooks/setResult";
 
 export default function Questions({ onChecked }) {
-  // const [checked, setChecked] = useState(undefined);
-  // const { trace } = useSelector((state) => state.questions);
-  // const result = useSelector((state) => state.result.result);
-  // const [{ isLoading, apiData, serverError }] = useFetchQuestion();
-  // useSelector((state) => console.log(state));
-  // const question = data[0];
-  // const questions = useSelector(
-  //   (state) => state.questions.queue[state.questions.trace]
-  // );
-  // const dispatch = useDispatch();
-  // //const trace = useSelector((state) => state.questions.trace);
-
-  // useEffect(() => {
-  //   dispatch(updateResult({ trace, checked }));
-  // }, [checked]);
-
-  // // useEffect(() => {
-  // //   // console.log(isLoading );
-  // //   // console.log(apiData);
-  // //   //console.log(serverError );
-  // // });
-
-  // function onSelect(i) {
-  //   console.log(i);
-  //   onChecked(i);
-  //   setChecked(i);
-  //   dispatch(updateResult({ trace, checked }));
-  // }
   const [checked, setChecked] = useState(undefined);
   const { trace } = useSelector((state) => state.questions);
   const result = useSelector((state) => state.result.result);
