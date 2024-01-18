6396cbc2df1736382f77900a196dcaa88b84c775
viki2207 <vidhishah2207@gmail.com>
2024-01-18 18:27:58 +1100

    analytics module

--- Projects/Url_shortner_node/controllers/url.js
+++ Projects/Url_shortner_node/controllers/url.js
@@ -13,4 +13,13 @@ async function handleGenerateNewShortURL(req, res) {
   });
   return res.json({ id: shortID });
 }
-module.exports = { handleGenerateNewShortURL };
+
+async function handleGetAnalytics(req, res) {
+  const shortId = req.params.shortId;
+  const result = await URL.findOne({ shortId });
+  return res.json({
+    totalClicks: result.visitHistory.length,
+    analytics: result.visitHistory,
+  });
+}
+module.exports = { handleGenerateNewShortURL, handleGetAnalytics };
