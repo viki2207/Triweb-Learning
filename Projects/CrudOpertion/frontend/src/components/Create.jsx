8e68c928529e78b7288f4fff5449f09cdb142d23
viki2207 <vidhishah2207@gmail.com>
2024-02-13 18:13:38 +1100

    create post page

--- Projects/CrudOpertion/frontend/src/components/Create.jsx
+++ Projects/CrudOpertion/frontend/src/components/Create.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
-const create = () => {
-  return <div>create</div>;
+const Create = () => {
+  return (
+    <div className="container my-2">
+      <h2 className="text-center"> Enter the Data</h2>
+
+      <form>
+        <div className="mb-3">
+          <label className="form-label">Name</label>
+          <input type="email" className="form-control" />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Email address</label>
+          <input type="email" className="form-control" />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Password</label>
+          <input type="password" className="form-control" />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
-export default create;
+export default Create;
